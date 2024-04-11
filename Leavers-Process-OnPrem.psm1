ed61b1286beb47055f97bc83d44c9cebb9528743
James Tarran <jtarran@techary.com>
2024-04-11 13:00:21 +0100

    Update Leavers-Process-OnPrem.psm1
    - Further changes to get-stringhash

--- Leavers-Process-OnPrem.psm1
+++ Leavers-Process-OnPrem.psm1
@@ -59,7 +59,7 @@ function invoke-leaverprocess {
         }
         $stringAsStream = [System.IO.MemoryStream]::new()
         $writer = [System.IO.StreamWriter]::new($stringAsStream)
-        $writer.write($input)
+        $writer.write($string)
         $writer.Flush()
         $stringAsStream.Position = 0
         Get-FileHash -InputStream $stringAsStream | Select-Object Hash
